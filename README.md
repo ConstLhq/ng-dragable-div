ba8c6624a02c5822822d9b4a489dd0a3579645d3
ConstLhq <const_lhq@126.com>
2016-12-03 21:30:36 +0800

    create

---
+++ README.md
@@ -0,0 +1,3 @@
+###Dragable div directive for angular 1.x
+##### implements a directive to display whatever you want to with the ability to be draged freely. 
+##### See more detail in example file   
---
+++ angular-dragable-div.js
@@ -0,0 +1,92 @@
+angular.module('angular-dragable-div', [])
+	.directive("dragAble", function() {
+		return {
+			restrict: 'EA',
+			transclude: false,
+			scope: {
+				ddShow: '=?',
+				ddClass: '@',
+				ddHeight: '@',
+				ddWidth: '@',
+				ddZindex: '@',
+				ddBackground: '@',
+			},
+			link: function($scope, element, attrs) {
+				var param = {};
+				param.ddShow = $scope.ddShow !== 'false';
+				param.height = $scope.ddHeight || '300px';
+				param.width = $scope.ddWidth || '500px';
+				param.zindex = $scope.ddZindex || 1000;
+				param.className = $scope.ddClass || 'ng-dragable';
+				param.background = $scope.ddBackground || 'rgba(170,242,255,0.5)';
+				element.addClass(param.className);
+				var body = document.body;
+				dragable = element[0];
+				dragable.style.width = param.width;
+				dragable.style.height = param.height;
+				dragable.style.zIndex = param.zindex;
+				dragable.style.position = 'absolute';
+				dragable.style.background = param.background;
+				dragable.style.cursor = 'move';
+				if ($scope.ddShow) {
+					dragable.style.display = "block";
+				} else {
+					dragable.style.display = "none";
+				}
+				if (dragable.tagName.toLowerCase() !== 'div' &&
+					dragable.tagName.toLowerCase() !== 'dragable') {
+					throw new Error('drag-able can only be applied to <div> or <drag-able> elements');
+				}
+				if (dragable.children.length === 0) {
+					throw new Error('You need to have content inside the <dragable>');
+				}
+
+				var closeIcon = document.createElement("a")
+				closeIcon.style.float = "right";
+				closeIcon.style.marginRight = "10px";
+				closeIcon.style.fontSize = "20px";
+				closeIcon.style.textDecoration = "none";
+				closeIcon.style.color = "#f22";
+				closeIcon.innerHTML = "×";
+				closeIcon.setAttribute("href", "#");
+				closeIcon.onclick = function(e) {
+					console.log("click")
+					e.preventDefault();
+					$scope.ddShow = false
+					$scope.$apply()
+				}
+				dragable.insertBefore(closeIcon, dragable.firstChild)
+				body.appendChild(dragable);
+				var clicked = "N";
+				var mausx = mausy = winx = winy = difx = dify = 0;
+				angular.element(document.querySelector("html")).on('mousemove', function(event) {
+					mausx = event.pageX;
+					mausy = event.pageY;
+					winx = document.querySelector("." + param.className).offsetLeft;
+					winy = document.querySelector("." + param.className).offsetTop;
+					if (clicked == "N") {
+						difx = mausx - winx;
+						dify = mausy - winy;
+					}
+					var newx = event.pageX - difx - angular.element(document.querySelector("." + param.className)).css("marginLeft");
+					var newy = event.pageY - dify - angular.element(document.querySelector("." + param.className)).css("marginTop");
+					angular.element(document.querySelector("." + param.className)).css("left", newx + "px");
+					angular.element(document.querySelector("." + param.className)).css("top", newy + "px");
+				});
+				angular.element(document.querySelector("." + param.className)).on("mousedown", function(event) {
+					clicked = "Y";
+				});
+				angular.element(document.querySelector("." + param.className)).on("mouseup", function(event) {
+					clicked = "N";
+				});
+
+				$scope.$watch('ddShow', function(value) {
+					if (!!value) {
+						dragable.style.display = "block";
+					} else {
+						dragable.style.display = "none";
+					}
+				});
+			}
+		}
+	})
